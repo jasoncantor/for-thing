93701264eaa113fad54969431f4306b33769e3aa
Jason Cantor <jason.cantor@stewardschool.org>
2019-11-19 10:45:43 -0500

    Update logdna.sh

--- logdna.sh
+++ logdna.sh
@@ -1,7 +1,7 @@
 echo "deb https://repo.logdna.com stable main" | sudo tee /etc/apt/sources.list.d/logdna.list
 wget -O- https://repo.logdna.com/logdna.gpg | sudo apt-key add -
 sudo apt-get update
-sudo apt-get install logdna-agent < "/dev/null" 
+sudo apt-get install logdna-agent 
 sudo logdna-agent -k 3c8ab342b9a87b9042f23a444886db1c
 sudo logdna-agent -t ethan
 sudo update-rc.d logdna-agent defaults
